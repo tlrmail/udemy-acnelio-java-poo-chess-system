fc0ef9a8cabc5982b2129c68de4f60186a4e6be0
thiago.leite <thiago.leite@dataprev.gov.br>
2024-03-05 16:35:37 -0300

    First Class Position

--- chess-system/src/application/Sistema.java
+++ chess-system/src/application/Sistema.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Sistema {
 
 	public static void main(String[] args) {
-		
+		Position position = new Position(3, 5);
+		System.out.println(position);
 	}
 }
