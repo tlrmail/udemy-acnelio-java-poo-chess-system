2c735fca908b4d268e95d7ddb0c7d28c1adb3ce6
thiago.leite <thiago.leite@dataprev.gov.br>
2024-03-07 15:12:25 -0300

    Possible moves os a piece

--- chess-system/src/boardgame/Piece.java
+++ chess-system/src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -10,13 +10,27 @@ public class Piece {
 		this.position = null;
 	}
 
+	public abstract boolean[][] possibleMoves(); 
+	
+	public boolean possibleMove() {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
 	protected Board getBoard() {
 		return board;
 	}
 	
-	
-	
-	
-	
-	
+	public boolean isThereAnyPossibleMove() {
+		boolean mat[][] = possibleMoves();
+		
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+		
+	}
 }
--- chess-system/src/chess/pieces/Rook.java
+++ chess-system/src/chess/pieces/Rook.java
@@ -12,6 +12,12 @@ public class Rook extends ChessPiece{
 	}
 	
 	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+	
+	@Override
 	public String toString() {
 		return "R";
 	}
